d15c514a2f4aa3319156c599a9241fe78cdb90b3
Stanislav Dolgachov <stanislav_dolgachov@epam.com>
2024-05-20 13:57:35 +0300

    Init commit

---
+++ olcEngine3d.cpp
@@ -0,0 +1,94 @@
+// olcEngine3d.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h";
+using namespace std;
+
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct tringle 
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<tringle> tris;
+};
+
+
+class olcEngine3D : public olc::PixelGameEngine
+{
+public: 
+    olcEngine3D()
+    {
+        sAppName = "3D Demo";
+    }
+
+private:
+    mesh meshCube;
+
+public:
+    bool OnUserCreate() override
+    {
+        meshCube.tris = {
+            // SOUTH
+            { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+            { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+            // EAST                                                      
+            { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+            { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+            // NORTH                                                     
+            { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+            { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+            // WEST                                                      
+            { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+            { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+            // TOP                                                       
+            { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+            { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+            // BOTTOM                                                    
+            { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+            { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+        };
+        
+        return true;
+    }
+public:
+    bool OnUserUpdate(float fElapsedTime) override
+    {   
+        for (auto tri : meshCube.tris) {
+
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    olcEngine3D demo;
+
+    if (demo.Construct(256, 240, 4, 4))
+        demo.Start();
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
